1e9fa1c439a53bc7b9c81adc9828d0889fc4f6d6
ctch3ng <32386442+ctch3ng@users.noreply.github.com>
2018-02-27 20:26:14 +0800

    Create CloudSim_XQuartz.sh

---
+++ CloudSim_XQuartz.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+ 
+echo "Image: cloud_sum"
+echo "Alias: Cloud_Sim_4"
+
+# run xhost and allow connections from your local machine:
+
+ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+xhost + $ip
+
+docker run -it --name Cloud_Sim_4 -e DISPLAY=$ip:0 -v /tmp/.X11-unix:/tmp/.X11-unix cloud_sum
+
+xhost - $ip
+docker rm /Cloud_Sim_4
