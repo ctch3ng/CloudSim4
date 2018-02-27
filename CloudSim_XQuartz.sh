b9627680158f73b74d8650ea3c66b9e12b9ed2a6
ctch3ng <32386442+ctch3ng@users.noreply.github.com>
2018-02-27 22:17:40 +0800

    Update CloudSim_XQuartz.sh

--- CloudSim_XQuartz.sh
+++ CloudSim_XQuartz.sh
@@ -8,7 +8,7 @@ echo "Alias: Cloud_Sim_4"
 ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 xhost + $ip
 
-docker run -it --name Cloud_Sim_4 -e DISPLAY=$ip:0 -v /tmp/.X11-unix:/tmp/.X11-unix cloud_sum
+docker run -it --name Cloud_Sim_4 -e DISPLAY=$ip:0 -v /tmp/.X11-unix:/tmp/.X11-unix cloud_sim
 
 xhost - $ip
 docker rm /Cloud_Sim_4
